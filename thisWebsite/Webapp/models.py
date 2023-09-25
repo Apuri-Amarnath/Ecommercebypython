d567b509622e6e87cbedf2d8fdfd0ce9a057b4db
Amar-Lap <@Apuri-Amarnath>
2023-09-25 14:54:31 +0530

    displaying products in Table.

--- thisWebsite/Webapp/models.py
+++ thisWebsite/Webapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 class Person(models.Model):  # Updated class name to follow naming conventions
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
@@ -7,9 +6,11 @@ class Person(models.Model):  # Updated class name to follow naming conventions
         return self.first_name + self.last_name
 
 class Product(models.Model):  # Updated class name to follow naming conventions
-    title = models.CharField(max_length=225)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # Corrected upload_to path
+    product_title = models.CharField(max_length=225)
+    product_description = models.TextField()
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_brand = models.CharField(max_length=225)
+    product_images = models.ImageField(upload_to='products/')
 
     def __str__(self):
-        return self.title
+        return self.product_title
--- thisWebsite/Webapp/views.py
+++ thisWebsite/Webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def index(request):
     contextvars={
@@ -9,8 +9,9 @@ def index(request):
 def about(request):
     contextvars={}
     return render(request,"about.html",contextvars)
-def product(request):
-    contextvars={}
+def products(request):
+    products = Product.objects.all()
+    contextvars={'products':products}
     return render(request,'products.html',contextvars)
 def services(request):
     contextvars={}
